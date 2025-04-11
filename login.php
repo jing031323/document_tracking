bdeec416412a51dd9aa06b78c135ea65aab73fba
jing031323 <jingkenneth13@gmail.com>
2025-04-11 12:40:59 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,63 @@
+<?php 
+
+require("../config.php");
+
+if(isset($_POST['login']))
+{
+    $email      = escape_string($_POST['email']);
+    $password   = escape_string($_POST['password']);
+
+    $redirect = isset($_POST['redirectTo']) ? escape_string($_POST['redirectTo']) : "";
+
+    $sql = query("SELECT * FROM users WHERE email = '{$email}'");
+    confirm($sql);
+
+    if(row_count($sql) == 0)
+    {
+        set_message_alert("alert-danger", "fa-times", "Email not registered.");
+        redirect("../?login");
+    }
+    else
+    {
+        $row = fetch_assoc($sql);
+        
+        $sql_ud = query("SELECT * FROM user_details WHERE id = {$row['id']}");
+        confirm($sql_ud);
+        $udRow = fetch_assoc($sql_ud);
+
+        if(accountIsLocked($row['id']))
+        {
+            set_message_alert("alert-danger", "fa-times", "Your account is locked.");
+            redirect("../?login");
+        }
+        else
+        {
+            if(password_verify($password, $row['password']))
+            {
+                $_SESSION['user']    = $row['email'];
+                $_SESSION['user_id'] = $udRow['id'];
+                $_SESSION['unit']    = $udRow['ud_unit'];
+                $_SESSION['usrname'] = ucwords(strtolower($udRow['ud_name']));
+                $_SESSION['role']    = $row['role'];
+                $_SESSION['timestamp'] = time();
+
+                // dd($redirect);
+
+                if($redirect != "") {
+                    redirect("$redirect");
+                }
+                else {
+                    redirect("../dashboard/");
+                }
+
+            }
+            else
+            {
+                set_message_alert("alert-danger", "fa-times", "Incorrect Password.");
+                redirect("../?login&try={$email}");
+            }
+        }
+    }
+}
+
+?>
