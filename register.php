2c6b63dbef7b28f470c7cdfbdc461d63993312c1
jing031323 <jingkenneth13@gmail.com>
2025-04-11 12:42:46 +0800

    Add files via upload

---
+++ register.php
@@ -0,0 +1,42 @@
+<?php
+
+require("../config.php");
+
+if(isset($_POST['register']))
+{
+    // users.table
+    $email      = escape_string($_POST['email']);
+    $password   = escape_string($_POST['password']);
+    // user_details.table
+    $fullname   = escape_string($_POST['fullname']);
+    $unit       = escape_string($_POST['unit']);
+    $reset      = random_str(8);
+    $options = [
+        'cost' => 12,
+    ];
+
+    $check = query("SELECT * FROM users WHERE email = '{$email}'");
+    confirm($check);
+
+    if(row_count($check) >= 1)
+    {
+        set_message_alert("alert-danger", "fa-info-circle", "Email already in use.");
+        redirect("../?register");
+    }
+    else
+    {
+        $hashed_pwd = password_hash($password, PASSWORD_BCRYPT, $options);
+
+        $insert_users = query("INSERT INTO users (email, password, reset) VALUES ('{$email}', '{$hashed_pwd}', '{$reset}')");
+        $last_id = last_id();
+        confirm($insert_users);
+
+        $insert_user_d = query("INSERT INTO user_details (id, ud_unit, ud_name) VALUES ('{$last_id}', '{$unit}', '{$fullname}')");
+        confirm($insert_user_d);
+
+        set_message_alert("alert-success", "fa-check", "Account created! Sign in below.");
+        redirect("../?login");
+    }
+}
+
+?>
